4a2af82074e1d2017dfdc3c0d180fc0dd2066891
roshan051-pixel <92286701+roshan051-pixel@users.noreply.github.com>
2021-11-17 01:55:35 +0530

    Part 2: Solution

---
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName){
+        
+    	if(restaurants != null && restaurants.size() >0) {
+    		for(Restaurant res: restaurants) {
+    			if(res.getName().equalsIgnoreCase(restaurantName))
+    				return res;
+    		}
+    	}
+    	
+    	return null;
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+   
+}
